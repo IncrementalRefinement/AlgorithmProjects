e8b859a2b2f5da8709874648fe725cec92322be7
LukeLIN-web <1263810658@qq.com>
2022-03-13 17:03:04 +0800

    begin2

--- splayTree src/main.cpp
+++ splayTree src/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+struct Node {
+    int data;
+    Node *parent;
+    Node *left;
+    Node *right;
+};
+
+typedef Node * NodePtr;
+
+class SplayTree{
+private:
+    NodePtr root;
+
+
+};
+
+
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
