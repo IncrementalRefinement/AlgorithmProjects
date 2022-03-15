ab5c94238973d87fcd469536186541ae64b005a7
IncrementalRefinement <fluent_python@qq.com>
2022-03-15 20:23:08 +0800

    initialize AvlTree&Test

---
+++ Proj1/AvlTree.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Xander on 2022/3/15.
+//
+
+#include "AvlTree.h"
+
+AvlTree::AvlTreeNode::AvlTreeNode(int) {
+
+}
+
+AvlTree::AvlTree() {
+
+}
+
+void AvlTree::Delete(int) {
+
+}
+
+void AvlTree::Insert(int) {
+
+}
+
+bool AvlTree::Query(int) {
+    return false;
+}
+
+bool AvlTree::Query(AvlTree::AvlTreeNode *node, int key) {
+    return false;
+}
+
+AvlTree::AvlTreeNode *AvlTree::Delete(AvlTree::AvlTreeNode *node, int key) {
+    return nullptr;
+}
+
+AvlTree::AvlTreeNode *AvlTree::Insert(AvlTree::AvlTreeNode *node, int key) {
+    return nullptr;
+}
+
+int AvlTree::FindMinValue(AvlTree::AvlTreeNode *node) {
+    return 0;
+}
+
+AvlTree::AvlTreeNode *AvlTree::rotateIfNecessary(AvlTree::AvlTreeNode *node) {
+    return nullptr;
+}
+
+AvlTree::AvlTreeNode *AvlTree::leftRotate(AvlTree::AvlTreeNode *node) {
+    return nullptr;
+}
+
+AvlTree::AvlTreeNode *AvlTree::rightRotate(AvlTree::AvlTreeNode *node) {
+    return nullptr;
+}
---
+++ Proj1/AvlTreeTest.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Xander on 2022/3/15.
+//
+
+#include "AvlTree.h"
+
+int main() {}
