bbfc615f433a760e0f3ac48dba8d240105d402e1
LukeLIN-web <1263810658@qq.com>
2022-04-10 21:45:37 +0800

    add some explanation

--- Proj3/Solution2.cpp
+++ Proj3/Solution2.cpp
@@ -1,10 +1,16 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 const int maxn = 1005;
 int no[maxn], id[maxn];
 int vertexNum, res = -1, cost;
 int G[maxn][maxn], cnt[maxn], group[maxn], vis[maxn], p[maxn];
 
+// vis 当前最大团是哪些点
+// ans 当前团size
+//group 保存了答案中最大团的点
+// cnt  从这个点往后搜索, 最大答案的size
+//
 void dfs(int u, int ans, int t_cost) {
     for (int i = u + 1; i <= vertexNum; i++) {
         // 花费过大时返回. 
@@ -29,9 +35,9 @@ void dfs(int u, int ans, int t_cost) {
 
 void maxclique() {
     for (int i = vertexNum; i > 0; i--) {
-        vis[0] = i;
+        vis[0] = i;// 假设大小为1的最大团开始搜索, 这个团
         dfs(i, 1, p[i]);// 查询V,有一个顶点, 花费p[i]价格
-        cnt[i] = res;
+        cnt[i] = res;// 从这个点往后答案就是res
     }
 }
 
@@ -39,10 +45,11 @@ int main() {
 #ifdef MyTest
     freopen("Sakura.txt", "r", stdin);
 #endif
-    int edgeNum, pos = 1;
+    int edgeNum;// 当前最大团大小为1
     scanf("%d %d", &edgeNum, &vertexNum);
     memset(G, -1, sizeof(G));
     vector<pair<int, int> > edges(edgeNum);
+    int pos = 1;
     for (int i = 0; i < edgeNum; i++)
         scanf("%d %d", &edges[i].first, &edges[i].second);
     for (int i = 0; i < vertexNum; i++) {
