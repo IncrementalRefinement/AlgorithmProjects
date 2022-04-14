9a85e1b56591681927f52df80dfa5d8cb30fe33b
GYgth <2401227794@qq.com>
2022-04-14 10:30:58 +0800

    p3

--- Proj3/dataset/test.c
+++ Proj3/dataset/test.c
@@ -19,7 +19,7 @@ int main(void)
     int num1[i],num2[i];
     struct tip tips[i];
     struct fruitprice price[M];
-	if((fp=fopen(".\\Data\\100sparse.txt","w"))==NULL)
+	if((fp=fopen(".\\Data\\20sparse.txt","w"))==NULL)
 	{
 	printf("can not open the file\n");
 	exit(0);
