7697cd936c5f96519bd78c3f807ee9d438f4dd25
Xander <fluent_python@qq.com>
2022-06-07 14:26:01 +0800

    initialize the test runnable

---
+++ Proj8/project8/src/main/java/com/demo/app/diy/mapreduce/Mapper.java
@@ -0,0 +1,4 @@
+package com.demo.app.diy.mapreduce;
+
+public class Mapper {
+}
---
+++ Proj8/project8/src/main/java/com/demo/app/diy/mapreduce/TestRunnable.java
@@ -0,0 +1,13 @@
+package com.demo.app.diy.mapreduce;
+
+public class TestRunnable implements Runnable {
+
+    private static final long MAX = 1000000000;
+
+    @Override
+    public void run() {
+        for (long i = 0; i < MAX; i++) {
+
+        }
+    }
+}
