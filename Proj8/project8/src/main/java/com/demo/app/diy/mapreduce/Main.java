2db82f212cab324f33c0f2e400e39cb85f4406d4
LTakamori <fluent_python@qq.com>
2022-06-07 14:43:48 +0800

    yes we can

--- Proj8/project8/src/main/java/com/demo/app/diy/mapreduce/Main.java
+++ Proj8/project8/src/main/java/com/demo/app/diy/mapreduce/Main.java
@@ -1,11 +1,22 @@
 package com.demo.app.diy.mapreduce;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
-            TestRunnable t = new TestRunnable();
-            t.run();
-        }
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        System.out.println("Main begins");
+        TestRunnable t1 = new TestRunnable();
+        TestRunnable t2 = new TestRunnable();
+        TestRunnable t3 = new TestRunnable();
+        TestRunnable t4 = new TestRunnable();
+        executorService.execute(t1);
+        executorService.execute(t2);
+        executorService.execute(t3);
+        executorService.execute(t4);
+        System.out.println();
+        System.out.println("Main ends");
     }
 }
--- Proj8/project8/src/main/java/com/demo/app/diy/mapreduce/TestRunnable.java
+++ Proj8/project8/src/main/java/com/demo/app/diy/mapreduce/TestRunnable.java
@@ -3,11 +3,14 @@ package com.demo.app.diy.mapreduce;
 public class TestRunnable implements Runnable {
 
     private static final long MAX = 1000000000;
+    private static long item = 0;
 
     @Override
     public void run() {
-        for (long i = 0; i < MAX; i++) {
+        System.out.println("thread begins");
+        while (true) {
 
         }
+        // System.out.println("thread ends");
     }
 }
